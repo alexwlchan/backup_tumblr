5fada67956d8c81d3f8e2c7165cd0e4ecd3b04e9
Alex Chan <alex@alexwlchan.net>
2018-12-05 21:10:53 +0000

    Get some more debugging info if/when the media script fails

--- save_media_files.py
+++ save_media_files.py
@@ -17,7 +17,11 @@ from sinkingship import find_all_metadata_files, save_post_media_files
 def save_all_media_files(metadata):
     all_media_files = list(find_all_metadata_files(path=metadata))
     for info_path in tqdm.tqdm(all_media_files):
-        save_post_media_files(info_path)
+        try:
+            save_post_media_files(info_path)
+        except Exception:
+            print(f"Error trying to save post {os.path.basename(info_path)}")
+            raise
 
 
 if __name__ == '__main__':
